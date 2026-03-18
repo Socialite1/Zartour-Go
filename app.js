cc186525d1bf1138764c1877bb0cd34a427307d5
Socialite1 <tgsekgololo@gmail.com>
2026-03-18 11:18:34 +0200

    Update Supabase URL in app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const supabase = supabase.createClient(
-  "YOUR_SUPABASE_URL",
+  "https://fzzxcshdetqruxquxrbe.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ6enhjc2hkZXRxcnV4cXV4cmJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM4MjIyMDgsImV4cCI6MjA4OTM5ODIwOH0.5BjEQWW8n7k62Uw7_JhsmZlA9Anl-mKqFiWs2Wwlilc"
 );
 
